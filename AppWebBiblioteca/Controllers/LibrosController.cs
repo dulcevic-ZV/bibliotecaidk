8afebf6e1156d8d852c6afec5866fd0ab69eab3a
Dulce <muffinwithdarkchocolate777@gmail.com>
2026-07-17 22:38:16 -0600

    Agregar archivos de proyecto.

---
+++ AppWebBiblioteca/Controllers/LibrosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppWebBiblioteca.Models;
+
+namespace AppWebBiblioteca.Controllers
+{
+    public class LibrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Libro> libros = new List<Libro>()
+            {
+                new Libro
+                {
+                    Id = 1,
+                    Title= "Drácula",
+                    Autor= "Bram Stoker",
+                    Category= "Novela",
+                    Price = 15.90,
+                    Available = false
+                },
+                new Libro
+                {
+                    Id = 2,
+                    Title= "Witch Hat Atelier", 
+                    Autor= "Kamome Shirahama", //THE GOAT
+                    Category= "Manga",
+                    Price = 18.00,
+                    Available = true
+                }
+            };
+            return View(libros);
+        }
+    }
+}
---
+++ AppWebBiblioteca/Models/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace AppWebBiblioteca.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public bool Available { get; set; }
+
+    }
+}
