c4f33472195b391999b2f2b76246a41695e36554
Dulce <muffinwithdarkchocolate777@gmail.com>
2026-08-20 22:34:01 -0600

    semana 5

---
+++ AppWebBiblioteca/Models/AcordionService.cs
@@ -0,0 +1,12 @@
+﻿namespace AppWebBiblioteca.Models
+{
+    public class AcordionService
+    {
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        public string Titulo2 { get; set; }
+        public string Texto2 { get; set; }
+        public string Titulo3 { get; set; }
+        public string Texto3 { get; set; }
+    }
+}
---
+++ AppWebBiblioteca/Models/serviceCard.cs
@@ -0,0 +1,8 @@
+﻿namespace AppWebBiblioteca.Models
+{
+    public class serviceCard
+    {
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+    }
+}
